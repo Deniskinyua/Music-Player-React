43aedc6817de90e040637b64da2e21119758d85b
Deniskinyua <kimenjudenis@gmail.com>
2022-07-28 06:32:53 +0300

    updates

---
+++ my-music-player/src/Player/audios.js
@@ -0,0 +1,25 @@
+exports.songsdata = [
+    {
+      'title': "Baby Riddim -FAVE",
+      'url':''
+    },
+    {
+      name: "Malkia - Sauti Sol",
+      'url':''
+    },
+    {
+      name: "Get Down - ChidBenz ft Nonini",
+      'url': ''
+    },
+    {
+      name: "Leo ni Leo - E-Sir",
+      'url':''
+    },
+    {
+      name: "Mos Mos -E-sir",
+      'url':''
+    },
+    {
+      name: "Watoto Na Pombe - Mejja ft Otile Brown, Magixx Enga",
+      'url':''}
+  ];
